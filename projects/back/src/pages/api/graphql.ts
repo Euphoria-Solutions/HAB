1d5ea5486723e53f405201e3589a797d4926fdf5
tugsuu itgel <itgeltugsjargal421@gmail.com>
2024-02-21 17:42:50 +0800

    feat(setup back-end project): back-end
    setup back-end project for hab

---
+++ projects/back/src/pages/api/graphql.ts
@@ -0,0 +1,60 @@
+import { typeDefs } from '../../graphql/types';
+import { allResolvers } from '../../resolvers';
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import { InMemoryLRUCache } from '@apollo/utils.keyvaluecache';
+import { ApolloServer } from 'apollo-server-cloud-functions';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import { NextFunction, Request, Response } from 'express';
+
+const server = new ApolloServer({
+  schema: buildSubgraphSchema({
+    typeDefs: typeDefs,
+    resolvers: allResolvers,
+  }),
+  csrfPrevention: true,
+  cache: new InMemoryLRUCache(),
+  context: ({ req, res }: { req: Request; res: Response }) => ({
+    headers: req.headers,
+    req,
+    res,
+  }),
+  introspection: true,
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const uri =
+  process.env[
+    `MONGODB_URL_${process.env.CURRENT_ENV == 'PROD' ? 'PROD' : 'DEV'}`
+  ] ?? '';
+
+mongoose.connect(uri);
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log(
+    `MongoDB database connection established successfully with ENVIRONMENT: ${process.env.ENV}`
+  );
+});
+
+const graphqlHandler = server.createHandler();
+
+const handler = async (req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  bodyParser.json({ limit: '50mb' })(req, res, () => {
+    graphqlHandler(req, res, next);
+  });
+};
+
+export default handler;
---
+++ projects/back/src/pages/index.tsx
@@ -0,0 +1,10 @@
+const Index = () => {
+  return (
+    <div>
+      Hello from forexplore backend API with
+      {process.env.CURRENT_ENV == 'PROD' ? 'PROD' : 'DEV'} Environment
+    </div>
+  );
+};
+
+export default Index;
